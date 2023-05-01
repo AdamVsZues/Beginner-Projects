ee05bbea447c820cc0d60f805a3f0e093b7877f3
AdamVsZues <115662796+AdamVsZues@users.noreply.github.com>
2023-05-01 15:06:39 -0500

    Delete Qr codes directory

--- Qr codes/decode/decode_qr.py
+++
@@ -1,8 +0,0 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
-
-img = Image.open('C:/Users/Tyant/Desktop/Projects/My Qr codes/python25projects.png')
-
-results = decode(img)
-
-print(results)
--- Qr codes/encode/QR.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-
-data = "https://www.youtube.com/@JoeButter42"
-
-img = qrcode.make(data)
-
-img.save('C:/Users/Tyant/Desktop/Projects/My Qr codes/TheBestChannel.png')
